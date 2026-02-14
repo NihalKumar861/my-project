9588b4e2f509f9f9aed7ce541baf335486dba7a0
NihalKumar861 <nihalkum56@gmail.com>
2026-02-14 15:03:06 +0500

    import tensorflow as tf.py

--- import tensorflow as tf.py
+++ import tensorflow as tf.py
@@ -50,6 +50,6 @@ predictions = model.predict(x_test)
 
 plt.imshow(x_test[0], cmap=plt.cm.binary)
 
-plt.title(f"Predicted: {predictions[0].argmax()}")
+plt.title(f" My Predicted: {predictions[0].argmax()}")
 
 plt.show()
